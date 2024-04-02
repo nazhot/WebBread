598004cdaf545b17b0b3044acdf08c239152b72d
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-02 11:56:57 -0600

    Starting to Draw Holes

--- board.js
+++ board.js
@@ -1,7 +1,7 @@
 const boardTypes = Object.freeze({
-    FULL: {id: 0, rows: 60, cols: 5},
-    HALF: {id: 1, rows: 30, cols: 5},
-    MINI: {id: 2, rows: 15, cols: 5}
+    FULL: {id: 0, rows: 60, cols: 5, powerGroups: 10},
+    HALF: {id: 1, rows: 30, cols: 5, powerGroups: 5},
+    MINI: {id: 2, rows: 15, cols: 5, powerGroups: 1}
 });
 
 const boardIds = ["Full", "Half", "Mini"];
@@ -14,12 +14,18 @@ class Board {
         this.type = boardIds[boardType.id];
         this.rows = boardType.rows;
         this.cols = boardType.cols;
+        this.powerGroups = boardType.powerGroups;
+        this.border = .01 * w;
         this.x = x;
         this.y = y;
         this.w = w;
         this.l = l;
+        this.holeAreaLength = l - this.border * 2;
         this.powerWidth = w / 6.0;
+        this.powerLength = l - ( 1 + this.powerGroups ) * this.border;
         this.centerWidth = this.powerWidth * 4.0;
+        this.holeSize = ( this.powerWidth - this.border * 2.5 ) / 2;
+        console.log(this.holeSize);
     }
 
     draw() {
@@ -32,6 +38,12 @@ class Board {
         //drawing power sections
         rect( this.x, this.y, this.l, this.powerWidth );
         rect( this.x, this.y + this.w, this.l, -this.powerWidth );
+
+        fill( 255, 0, 0 );
+        for ( let i = 0; i < this.rows; ++i ) {
+            rect( this.x + this.border + i * ( this.border / 2 + this.holeSize ),
+                  this.y + this.powerWidth + this.border, this.holeSize, this.holeSize );
+        }
     }
 
 }
